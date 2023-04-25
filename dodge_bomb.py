90e9f4a9d96a4cf0a9a5cfbe385b785e635122ed
Yuuto <c0a22162ef@edu.teu.ac.jp>
2023-04-25 17:10:59 +0900

    演習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,13 +1,16 @@
 import random
 import pygame as pg
 import sys
+import time
 
+#矢印で移動させるための辞書
 delta = {
         pg.K_UP:(0,-1),
         pg.K_DOWN:(0,+1),
         pg.K_LEFT:(-1,0),
-        pg.K_RIGHT:(+1,0)
+        pg.K_RIGHT:(+1,0),
         }
+
 
 def check_bound(scr_rct, obj_rct):
     """
@@ -23,6 +26,7 @@ def check_bound(scr_rct, obj_rct):
         tate = False
     return yoko,tate
 
+#こうかとんの画像
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -32,7 +36,7 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900,400
-
+#ボムの画像
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
     bb_img.set_colorkey((0, 0, 0)) 
@@ -49,17 +53,18 @@ def main():
                 return 0
 
         tmr += 1
-
+        
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
-
+        
         if check_bound(screen.get_rect(),kk_rct) !=(True,True):
             for k, mv in delta.items():
                 if key_lst[k]:
                     kk_rct.move_ip(-mv[0], -mv[1])
-
+        
+        
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img,kk_rct) #練習４
         bb_rct.move_ip(vx, vy) #練習３
@@ -70,6 +75,18 @@ def main():
             vy *= -1
         screen.blit(bb_img,bb_rct) #練習３
 
+        kk_img8 = pg.image.load("fig/8.png")
+        kk_img8 = pg.transform.rotozoom(kk_img8, 0, 2.0)
+        t = 0
+        if kk_rct.colliderect(bb_rct):
+            if t != 5:
+                kk_img = kk_img8 #演習３　処理が終わらない
+                
+                
+
+
+        
+        
         pg.display.update()
         clock.tick(1000)
 
