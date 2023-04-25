d663dc175a61bf53ebd482b919a0163da80d74e2
Yuuto <c0a22162ef@edu.teu.ac.jp>
2023-04-25 14:22:20 +0900

    ランダム

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import pygame as pg
 import sys
 
@@ -11,6 +12,9 @@ def main():
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
     bb_img.set_colorkey((0, 0, 0))
+    x = random.randint(0, 1600)
+    y = random.randint(0, 900)
+    screen.blit(bb_img, (x,y))
     tmr = 0
 
     while True:
@@ -21,7 +25,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [600, 200])
+        screen.blit(bb_img, [x, y])
 
         pg.display.update()
         clock.tick(1000)
