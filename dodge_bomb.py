ee44be9c6445cef202044955f3cefe0cf7ab5fe9
Yuuto <c0a22162ef@edu.teu.ac.jp>
2023-04-25 15:12:33 +0900

    こうかとん操作

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,13 @@ import random
 import pygame as pg
 import sys
 
+delta = {
+        pg.K_UP:(0,-1),
+        pg.K_DOWN:(0,+1),
+        pg.K_LEFT:(-1,0),
+        pg.K_RIGHT:(+1,0)
+        }
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -9,10 +16,12 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
 
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
-    bb_img.set_colorkey((0, 0, 0)) #関数1
+    bb_img.set_colorkey((0, 0, 0)) 
     x,y = random.randint(0, 1600),random.randint(0, 900)
     screen.blit(bb_img, (x,y))
     vx,vy = -1, +1
@@ -26,8 +35,15 @@ def main():
                 return 0
 
         tmr += 1
+
+        key_lst = pg.key.get_pressed()
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img,kk_rct)
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img,bb_rct)
 
