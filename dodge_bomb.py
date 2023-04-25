72e2d08893506d4439d0074946b4e80ead3edfef
Yuuto <c0a22162ef@edu.teu.ac.jp>
2023-04-25 14:33:40 +0900

    爆弾の移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,12 +9,15 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
-    bb_img.set_colorkey((0, 0, 0))
-    x = random.randint(0, 1600)
-    y = random.randint(0, 900)
+    bb_img.set_colorkey((0, 0, 0)) #関数1
+    x,y = random.randint(0, 1600),random.randint(0, 900)
     screen.blit(bb_img, (x,y))
+    vx,vy = -1, +1
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x,y
     tmr = 0
 
     while True:
@@ -25,7 +28,8 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [x, y])
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img,bb_rct)
 
         pg.display.update()
         clock.tick(1000)
