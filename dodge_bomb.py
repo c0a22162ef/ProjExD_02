a780ec9eecf228fd78e729213ba735d8ae38df55
Yuuto <c0a22162ef@edu.teu.ac.jp>
2023-04-25 18:05:15 +0900

    コメント #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -26,8 +26,9 @@ def check_bound(scr_rct, obj_rct):
         tate = False
     return yoko,tate
 
-#こうかとんの画像
+
 def main():
+    #こうかとんの画像
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
@@ -36,7 +37,8 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900,400
-#ボムの画像
+
+    #ボムの画像
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
     bb_img.set_colorkey((0, 0, 0)) 
@@ -58,15 +60,16 @@ def main():
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
-        
+                
+        #画面枠で反射
         if check_bound(screen.get_rect(),kk_rct) !=(True,True):
             for k, mv in delta.items():
                 if key_lst[k]:
                     kk_rct.move_ip(-mv[0], -mv[1])
         
         
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img,kk_rct) #練習４
+        screen.blit(bg_img, [0, 0]) #ボム画像
+        screen.blit(kk_img,kk_rct) #練習４　動くこうかとん
         bb_rct.move_ip(vx, vy) #練習３
         yoko,tate = check_bound(screen.get_rect(),bb_rct)
         if not yoko:
@@ -75,16 +78,11 @@ def main():
             vy *= -1
         screen.blit(bb_img,bb_rct) #練習３
 
-        kk_img8 = pg.image.load("fig/8.png")
-        kk_img8 = pg.transform.rotozoom(kk_img8, 0, 2.0)
-        t = 0
+        kk_img8 = pg.image.load("fig/8.png") #8.pngのこうかとんをロード
+        kk_img8 = pg.transform.rotozoom(kk_img8, 0, 2.0) #8.pngのこうかとんのサイズ変更
+        
         if kk_rct.colliderect(bb_rct):
-            if t != 5:
-                kk_img = kk_img8 #演習３　処理が終わらない
-                
-                
-
-
+            kk_img = kk_img8 #演習３ 画像は変わるが処理が終わらない
         
         
         pg.display.update()
